2214da9c51a4f09d676b39399f6bbdca57956870
Martin Mystik Jonáš <martin.jonas@growjob.com>
2022-12-13 17:05:42 +0100

    Typo

--- src/Resolver/CallResolver/CalledClassResolver.php
+++ src/Resolver/CallResolver/CalledClassResolver.php
@@ -37,7 +37,7 @@ final class CalledClassResolver
                     return null;
                 }
                 return $classReflection->getName();
-            } elseif($calledClassName === 'self' || $calledClassName === 'static')  {
+            } elseif ($calledClassName === 'self' || $calledClassName === 'static')  {
                 return $classReflection->getName();
             }
             return $calledClassName;
