e488cc95f26859301c3778816fdbb2f59a5b03fc
Michal Lulco <lulco@efabrica.sk>
2023-01-11 16:40:42 +0100

    Fix: support for union type for custom form methods (#224)

--- tests/Rule/LatteTemplatesRule/PresenterWithoutModule/Source/CustomForm.php
+++ tests/Rule/LatteTemplatesRule/PresenterWithoutModule/Source/CustomForm.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace Efabrica\PHPStanLatte\Tests\Rule\LatteTemplatesRule\PresenterWithoutModule\Source;
 
 use Nette\Application\UI\Form;
+use Nette\Forms\Controls\TextArea;
 use Nette\Forms\Controls\TextInput;
 
 /**
  * @method CustomFormRenderer getRenderer()
- * @method TextInput addCustomText(string $name = 'custom_default', ?string $label = null, ?int $cols = null, ?int $maxLength = null)
+ * @method TextInput|TextArea addCustomText(string $name = 'custom_default', ?string $label = null, ?int $cols = null, ?int $maxLength = null)
  */
 class CustomForm extends Form
 {
