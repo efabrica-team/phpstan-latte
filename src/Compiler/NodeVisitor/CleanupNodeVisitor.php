e5b71ecf185675d6dd4fd4534b1d04dd54cf6f8c
Michal Lulco <lulco@efabrica.sk>
2023-01-13 15:56:43 +0100

    Remove if (false) from "switch" (#245)

--- src/Compiler/NodeVisitor/CleanupNodeVisitor.php
+++ src/Compiler/NodeVisitor/CleanupNodeVisitor.php
@@ -7,9 +7,12 @@ namespace Efabrica\PHPStanLatte\Compiler\NodeVisitor;
 use Efabrica\PHPStanLatte\Resolver\NameResolver\NameResolver;
 use PhpParser\Node;
 use PhpParser\Node\Expr\Closure;
+use PhpParser\Node\Expr\ConstFetch;
 use PhpParser\Node\Expr\FuncCall;
 use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\Stmt\Else_;
 use PhpParser\Node\Stmt\Expression;
+use PhpParser\Node\Stmt\If_;
 use PhpParser\Node\Stmt\TryCatch;
 use PhpParser\NodeTraverser;
 use PhpParser\NodeVisitorAbstract;
@@ -58,6 +61,32 @@ final class CleanupNodeVisitor extends NodeVisitorAbstract
                 $node->stmts = $closure->stmts;
                 return $node;
             }
+        }
+
+        if ($node instanceof If_) {
+            // replace if (false) to next condition
+            if (!($node->cond instanceof ConstFetch && $this->nameResolver->resolve($node->cond->name) === 'false')) {
+                return null;
+            }
+
+            $elseIfs = $node->elseifs;
+            $else = $node->else;
+            $firstElseIf = array_shift($elseIfs);
+            if ($firstElseIf === null) {
+                if (!$else instanceof Else_) {
+                    // no elseif nor else
+                    return NodeTraverser::REMOVE_NODE;
+                }
+                // only else
+                return $else->stmts;
+            }
+
+            $subnodes = [
+                'stmts' => $firstElseIf->stmts,
+                'elseifs' => $elseIfs,
+                'else' => $else,
+            ];
+            return new If_($firstElseIf->cond, $subnodes, $node->getAttributes());
         }
 
         return null;
