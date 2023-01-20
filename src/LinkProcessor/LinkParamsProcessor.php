d0f289a2b08856326b1d18072cd75aba92f8d763
Michal Lulco <lulco@efabrica.sk>
2023-01-20 11:25:30 +0100

    Ignore all named extra parameters in links check (#275)

--- src/LinkProcessor/LinkParamsProcessor.php
+++ src/LinkProcessor/LinkParamsProcessor.php
@@ -12,7 +12,6 @@ use PhpParser\Node\Expr\Array_;
 use PhpParser\Node\Expr\ArrayItem;
 use PhpParser\Node\Scalar\String_;
 use PHPStan\BetterReflection\BetterReflection;
-use ReflectionProperty;
 
 final class LinkParamsProcessor
 {
@@ -34,8 +33,18 @@ final class LinkParamsProcessor
             throw new InvalidArgumentException('Too many parameters');
         }
 
-        $transferredParams = [];
+        $reflectionClass = (new BetterReflection())->reflector()->reflectClass($class);
+        $reflectionMethod = $reflectionClass->getMethod($method);
+        if ($reflectionMethod === null) {
+            throw new InvalidArgumentException("Method $class::$method not found");
+        }
 
+        $methodParameters = [];
+        foreach ($reflectionMethod->getParameters() as $param) {
+            $methodParameters[] = $param->getName();
+        }
+
+        $transferredParams = [];
         if ($params !== []) {
             $paramValue = $params[0]->value;
             if (!$paramValue instanceof Array_) {
@@ -47,7 +56,12 @@ final class LinkParamsProcessor
                     continue;
                 }
                 $key = $arrayItem->key;
-                if ($key instanceof String_) {  // ignore key for now
+                if ($key instanceof String_) {
+                    $transferredParamName = $key->value;
+                    // Skip named params which are not method params - Nette adds them to query params (e.g. ?param1=foo&param2=bar) and not check if they are in method
+                    if (!in_array($transferredParamName, $methodParameters, true)) {
+                        continue;
+                    }
                     $arrayItem = new ArrayItem($arrayItem->value, null, $arrayItem->byRef, $arrayItem->getAttributes());
                     $transferredParams[$key->value] = new Arg($arrayItem);
                     continue;
@@ -56,18 +70,10 @@ final class LinkParamsProcessor
             }
         }
 
-        $reflectionClass = (new BetterReflection())->reflector()->reflectClass($class);
-        $reflectionMethod = $reflectionClass->getMethod($method);
-        if ($reflectionMethod === null) {
-            throw new InvalidArgumentException("Method $class::$method not found");
-        }
-
         $i = 0;
-        $methodParameters = [];
         foreach ($reflectionMethod->getParameters() as $param) {
             $name = $param->getName();
             $type = (string) $param->getType();
-            $methodParameters[] = $name;
             if (array_key_exists($i, $transferredParams)) {
                 $transferredParams[$name] = $transferredParams[$i];
                 unset($transferredParams[$i]);
@@ -83,19 +89,6 @@ final class LinkParamsProcessor
                 $transferredParams[$name] = new Arg(BuilderHelpers::normalizeValue([]));
             } else {
                 $transferredParams[$name] = new Arg(BuilderHelpers::normalizeValue(null));
-            }
-        }
-
-        // remove persistent parameters
-        foreach ($reflectionClass->getProperties(ReflectionProperty::IS_PUBLIC) as $property) {
-            if (in_array($property->getName(), $methodParameters, true)) {
-                continue;
-            }
-            if (preg_match_all('#[\s*]@persistent(?:\(\s*([^)]*)\s*\)|\s|$)#', (string) $property->getDocComment(), $m)) {
-                unset($transferredParams[$property->getName()]);
-            }
-            if (PHP_VERSION_ID >= 80000 && $property->getAttributesByInstance('Nette\Application\Attributes\Persistent')) {
-                unset($transferredParams[$property->getName()]);
             }
         }
 
