c099191a1955d4d3379550461f1433f243797ea8
Michal Lulco <lulco@efabrica.sk>
2023-04-24 14:14:07 +0200

    Fix sniffer and phpstan

--- src/Compiler/NodeVisitor/NotNullableSnippetDriverNodeVisitor.php
+++ src/Compiler/NodeVisitor/NotNullableSnippetDriverNodeVisitor.php
@@ -11,12 +11,10 @@ use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Expr\NullsafeMethodCall;
 use PhpParser\NodeVisitorAbstract;
 use PHPStan\Type\ObjectType;
-use PHPStan\Type\VerbosityLevel;
 
 final class NotNullableSnippetDriverNodeVisitor extends NodeVisitorAbstract implements ExprTypeNodeVisitorInterface
 {
     use ExprTypeNodeVisitorBehavior;
-
 
     public function enterNode(Node $node): ?Node
     {
@@ -25,8 +23,7 @@ final class NotNullableSnippetDriverNodeVisitor extends NodeVisitorAbstract impl
         }
 
         $callerType = $this->getType($node->var);
-        var_dump($callerType->describe(VerbosityLevel::typeOnly()));
-        if ($callerType instanceof ObjectType && $callerType->isInstanceOf('Nette\Bridges\ApplicationLatte\SnippetDriver')) {
+        if ($callerType instanceof ObjectType && $callerType->isInstanceOf('Nette\Bridges\ApplicationLatte\SnippetDriver')->yes()) {
             $methodCall = new MethodCall($node->var, $node->name, $node->args);
             $methodCall->setAttributes($node->getAttributes());
             return $methodCall;
