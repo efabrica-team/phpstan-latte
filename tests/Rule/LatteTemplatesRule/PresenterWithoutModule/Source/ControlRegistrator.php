f864fd61b7208a1f776d733562c520c983fb05f9
Michal Lulco <lulco@efabrica.sk>
2023-05-25 22:41:20 +0200

    Multi registered components

--- tests/Rule/LatteTemplatesRule/PresenterWithoutModule/Source/ControlRegistrator.php
+++ tests/Rule/LatteTemplatesRule/PresenterWithoutModule/Source/ControlRegistrator.php
@@ -4,12 +4,17 @@ declare(strict_types=1);
 
 namespace Efabrica\PHPStanLatte\Tests\Rule\LatteTemplatesRule\PresenterWithoutModule\Source;
 
+use Efabrica\PHPStanLatte\Tests\Rule\LatteTemplatesRule\PresenterWithoutModule\Fixtures\ComponentsPresenter;
 use Nette\Application\UI\Presenter;
 
 final class ControlRegistrator
 {
     public function register(Presenter $presenter)
     {
-        $presenter->addComponent(new SomeControl(), 'someControl');
+        if ($presenter instanceof ComponentsPresenter) {
+            $presenter->addComponent(new SomeControl(), 'someControl');
+        } else {
+            $presenter->addComponent(new SomeControl(), 'someControl');
+        }
     }
 }
